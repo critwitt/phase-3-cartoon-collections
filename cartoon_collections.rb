4a54dbd3ce147241d1abd9564f88060ab3252675
Chris Witte <christopher.t.witte@gmail.com>
2022-09-22 12:36:47 -0400

    commit tree 92274f2bd4de6152910ac11ee6ba912c652ea61a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each {|d| puts "#{dwarves.index(d) + 1}. #{d}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planateers
+  planateers.map {|p| "#{p.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  calls.map {|c| 
+    if c.length > 4
+      return true
+    end
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese foods
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |c|
+    if foods.include?(c)
+      return c
+    end
+  end
+  return nil
 end
